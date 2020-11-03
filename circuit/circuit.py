c7fde757d76075ae02b6d18060f431047d6d9e4a
wyc811504 <35424950+wyc811504@users.noreply.github.com>
2020-11-02 16:48:31 -0800

    Update circuit.py

--- circuit/circuit.py
+++ circuit/circuit.py
@@ -1046,6 +1046,51 @@ class Circuit:
 
         return fault_set
 
+    def pfs_multiple_separate(self, fname_tp, fname_log, mode="b"):
+        """ 
+        new pfs for multiple input patterns
+        the pattern list is obtained as a list consists of sublists of each pattern like:
+            input_file = [[1,1,0,0,1],[1,0,1,0,0],[0,0,0,1,1],[1,0,0,1,0]]
+        fault_list should be like the following format: (string in the tuples)
+            fault_list = [(1,0),(1,1),(8,0),(5,1),(6,1)]
+        """
+        if mode not in ["b", "x"]:
+            raise NameError("Mode is not acceptable")
+        # if os.path.exists('../data/fault_sim/') == False:
+        #     os.mkdir('../data/fault_sim/')
+        # input_path = '../data/modelsim/' + self.c_name + '/input/'
+        # if os.path.exists(input_path) == False:
+        #     os.mkdir(input_path)
+        # output_path = '../data/fault_sim/' + self.c_name + '/'
+        # if os.path.exists(output_path) == False:
+        #     os.mkdir(output_path)
+        fr = open(fname_tp, mode='r')
+        # output_path = output_path + fname.rstrip('tp_b.txt') + '_pfs_out.txt'
+        fw = open(fname_log, mode='w')
+        
+        lines = fr.readlines()
+        # obtain a multiple test patterns list from the input file
+        pattern_list = []
+        for line in lines[1:]:
+            line=line.rstrip('\n')
+            line_split=line.split(',')
+            for x in range(len(line_split)):
+                line_split[x]=int(line_split[x])
+            pattern_list.append(line_split)
+        for sub_pattern in pattern_list:
+            fault_subset = self.pfs_single(sub_pattern)
+            fault_sublist = list(fault_subset)
+            fault_sublist.sort(key=lambda x: ((x[0], x[1])))
+            pattern_str = map(str,sub_pattern)
+            pattern_str = ",".join(pattern_str)
+            fw.write(pattern_str + '\n')
+            for fault in fault_sublist:
+                fw.write(str(fault[0]) + '@' + str(fault[1]) + '\n')
+            fw.write('\n')
+        fr.close()
+        fw.close()
+        print("PFS-Separate completed. \nLog file saved in {}".format(fname_log))
+
 
     def pfs_multiple(self, fname_tp, fname_log, fname=None, mode="b"):
         """ 
