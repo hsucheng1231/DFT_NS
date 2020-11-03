ccb950bfdf31924f5bd89ee1408a5169d7792e65
ruochen627 <62487932+ruochen627@users.noreply.github.com>
2020-11-02 16:08:10 -0800

    new path

--- circuit/regular_tp_gen.py
+++ circuit/regular_tp_gen.py
@@ -7,10 +7,13 @@ def regular_tp_gen(circuit):
         num = len(circuit.PI)
         times = pow(2, num)
         pattern = []
-        output_path = os.path.join(config.PATTERN_DIR)
+        output_path = config.FAULT_SIM_DIR
         if not os.path.exists(output_path):
                 os.mkdir(output_path)
                 output_path = (output_path + '/' + circuit.c_name + '/')
+        if not os.path.exists(output_path):
+                os.mkdir(output_path)
+                output_path = (output_path + '/input/')
         if not os.path.exists(output_path):
                 os.mkdir(output_path)
         fw = open(output_path + circuit.c_name + '_full_tp_b.txt', mode='w')
